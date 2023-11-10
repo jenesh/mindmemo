5df0317f3ea4061ead0deaf4425de6756026373c
Jenesh Napit <jenesh.napit@gmail.com>
2023-11-10 13:00:16 -0500

    add /privacy page

--- index.js
+++ index.js
@@ -1,36 +1,88 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path'); 
-require('dotenv').config();
-require('./server/configure/passport_configure');
-const session = require('express-session');
-const userDetailRou = require('./server/routes/userDetails');
-const passport = require('passport');
-const cookieParser = require('cookie-parser');
-const cors = require('cors')
+const path = require("path");
+require("dotenv").config();
+require("./server/configure/passport_configure");
+const session = require("express-session");
+const userDetailRou = require("./server/routes/userDetails");
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } // Allow non-secure cookies for testing
-}));
-app.use(cors({origin: true, credentials: true}))
+const privacyPolicy = `
+Privacy Policy for MindMemo
+
+Last Updated: 11/10/2023
+
+1. Introduction:
+   Welcome to MindMemo. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our Google Chrome extension.
+
+2. Information We Collect:
+   - We may collect and store the following information:
+     - User email, profile information obtained through Google OAuth2
+
+3. How We Use Your Information:
+   - We use the collected information for purposes such as:
+     - Power your profile in the extension, user authentication and to map your account to your Google account
+
+4. Google OAuth2:
+   - Our extension uses Google OAuth2 for user authentication. By using our extension, you agree to Google's terms of service and privacy policy, which can be found at https://www.google.com/accounts/authsub/terms.html.
+
+5. Third-Party Services:
+   - We may use third-party services for analytics or other purposes. These services may collect information sent by your browser as part of a web page request.
+
+6. Cookies:
+   - We only store cookies that are necessary for the extension to function such as Google OAuth2 cookies for authentication and authorization.
+
+7. Security:
+   - We implement reasonable security measures to protect your information. However, no method of transmission over the internet or electronic storage is 100% secure.
+
+8. User Choices:
+   - You may choose to restrict the collection or use of your personal information. However, this may impact your ability to use certain features of the extension.
+
+9. Accessing, Updating, or Deleting Your Information:
+   - You can access, update, or request deletion of your information by contacting us at askmindmemo@gmail.com.
+   - We are working on a profile page for you to manage your information.
+
+10. Changes to This Privacy Policy:
+    - We may update our Privacy Policy from time to time. Any changes will be posted on this page.
+
+11. Contact Us:
+    - If you have any questions or concerns about our Privacy Policy, please contact us at askmindmemo@gmail.com
+
+By using MindMemo, you agree to the terms outlined in this Privacy Policy.
+
+Contact email: askmindmemo@gmail.com
+`;
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Allow non-secure cookies for testing
+  })
+);
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use('/userDetails', userDetailRou);
-app.use('/authRoutes', require('./server/routes/authRoutes'))
-app.get('/', (req, res) => {
-  res.send(`<a href = "/authRoutes/auth/google"> Authenticate with Google </a>`)
+app.use("/userDetails", userDetailRou);
+app.use("/authRoutes", require("./server/routes/authRoutes"));
+app.get("/", (req, res) => {
+  res.send(
+    `<a href = "/authRoutes/auth/google"> Authenticate with Google </a>`
+  );
+});
+
+app.get("/privacy", (req, res) => {
+  res.send(privacyPolicy);
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
-
-
--- server/configure/passport_configure.js
+++ server/configure/passport_configure.js
@@ -1,30 +1,31 @@
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const JwtStrategy = require('passport-jwt').Strategy
-const { PrismaClient } = require('@prisma/client');
-const {google} = require('googleapis');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const { PrismaClient } = require("@prisma/client");
+const { google } = require("googleapis");
 const prisma = new PrismaClient();
-require('dotenv').config();
+require("dotenv").config();
 
-const opts = {}
+const opts = {};
 
 opts.jwtFromRequest = (req) => {
-  let token = null
+  let token = null;
   if (req && req.cookies) {
-    token = req.cookies['jwt']
+    token = req.cookies["jwt"];
   }
   return token;
-}
+};
 
 opts.secretOrKey = process.env.secret_key;
-  
-  passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
     try {
       const user = await prisma.user.findUnique({
         where: { id: jwt_payload.user },
-        select: { id: true, email: true},
+        select: { id: true, email: true },
       });
-      
+
       if (!user) {
         return done(null, false); // User not found, authentication failed
       } else {
@@ -33,93 +34,91 @@ opts.secretOrKey = process.env.secret_key;
     } catch (err) {
       return done(err); // An error occurred, authentication failed
     }
-  }));
+  })
+);
 
-
-passport.use(new GoogleStrategy({
-    clientID: process.env.Google_Client_ID,
-    clientSecret: process.env.Google_Client_Secret,
-    callbackURL: "https://mymindmemo.com/authRoutes/google/callback",
-    accessType:'offline',
-    scope: ['https://www.googleapis.com/auth/calendar']
-    // "https://accounts.google.com/", 'https://www.googleapis.com/auth/calendar.readonly','https://www.googleapis.com/auth/plus.login'
-  },
-  async function (accessToken, refreshToken, profile, cb) {
-    console.log('------prof------')
-    console.log(refreshToken);
-    console.log(profile);
-    console.log('------------')
-    try {
-      let user = await prisma.User.findUnique({
-        where: {
-          googleId: profile.id,
-        },
-      });
-     
-
-      if (!user) {
-        const oAuth2Client = new google.auth.OAuth2(
-          process.env.Google_Client_ID,
-          process.env.Google_Client_Secret
-        );
-        const userTokens = {
-          access_token: accessToken,
-          refresh_token: refreshToken
-        };
-        oAuth2Client.setCredentials(userTokens);
-      
-        const tasks = google.tasks({ version: 'v1', auth: oAuth2Client });
-        const newTaskList = {
-          title: 'MINDMEMO'
-        };
-        const response = await tasks.tasklists.insert({ requestBody: newTaskList });
-        const createdTaskList = response.data;
-        user = await prisma.User.create({
-          data: {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.Google_Client_ID,
+      clientSecret: process.env.Google_Client_Secret,
+      callbackURL: "https://mymindmemo.com/authRoutes/google/callback",
+      accessType: "offline",
+      scope: ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/tasks"],
+      // "https://accounts.google.com/", 'https://www.googleapis.com/auth/calendar.readonly','https://www.googleapis.com/auth/plus.login'
+    },
+    async function (accessToken, refreshToken, profile, cb) {
+      console.log("------prof------");
+      console.log(refreshToken);
+      console.log(profile);
+      console.log("------------");
+      try {
+        let user = await prisma.User.findUnique({
+          where: {
             googleId: profile.id,
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            accessToken:accessToken,
-            refreshToken:refreshToken,
-            taskListId:createdTaskList.id
-          },
-        });
-      }
-      else {
-        await prisma.User.update({
-          where: { googleId: profile.id },
-          data: {
-            accessToken: accessToken,
-            refreshToken:refreshToken,
           },
         });
 
+        if (!user) {
+          const oAuth2Client = new google.auth.OAuth2(
+            process.env.Google_Client_ID,
+            process.env.Google_Client_Secret
+          );
+          const userTokens = {
+            access_token: accessToken,
+            refresh_token: refreshToken,
+          };
+          oAuth2Client.setCredentials(userTokens);
+
+          const tasks = google.tasks({ version: "v1", auth: oAuth2Client });
+          const newTaskList = {
+            title: "MINDMEMO",
+          };
+          const response = await tasks.tasklists.insert({
+            requestBody: newTaskList,
+          });
+          const createdTaskList = response.data;
+          user = await prisma.User.create({
+            data: {
+              googleId: profile.id,
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              accessToken: accessToken,
+              refreshToken: refreshToken,
+              taskListId: createdTaskList.id,
+            },
+          });
+        } else {
+          await prisma.User.update({
+            where: { googleId: profile.id },
+            data: {
+              accessToken: accessToken,
+              refreshToken: refreshToken,
+            },
+          });
+        }
+
+        console.log("------passportuser------");
+        console.log(user);
+        console.log("------------");
+
+        return cb(null, user);
+      } catch (error) {
+        return cb(error, null);
       }
-
-      console.log('------passportuser------')
-      console.log(user);
-      console.log('------------')
-
-      return cb(null, user);
-    } catch (error) {
-      return cb(error, null);
     }
-  }
-));
+  )
+);
 
 passport.serializeUser((user, done) => {
-    // Store user.id in the session
+  // Store user.id in the session
+  done(null, user);
+});
+// Retrieve user data based on user.id from the session
+passport.deserializeUser(async (user, done) => {
+  try {
     done(null, user);
-  });
-  // Retrieve user data based on user.id from the session
-  passport.deserializeUser(async (user, done) => {
-    try {  
-      done(null, user);
-    } catch (error) {
-      done(error, null);
-    }
-  });
-
- 
-  
-  
+  } catch (error) {
+    done(error, null);
+  }
+});
